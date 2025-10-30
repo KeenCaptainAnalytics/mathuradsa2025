7e90f6a0ca5d4e71504ba6bf13edbaca610e63fd
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-10-30 12:43:25 +0530

    Create Calculator.java

---
+++ batch3/binarysearch/java/Calculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Calculator {
+    static int add(int a , int b){
+        return a+b;
+    }
+    static int multiply(int a , int b){
+        return a*b;
+    }
+    static int diff(int a , int b){
+        return a-b;
+    }
+    static int div(int a , int b){
+        return a/b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first num");
+        int a = sc.nextInt();
+        System.out.println("Enter second number");
+        int b = sc.nextInt();
+        System.out.println("enter symbol (+,-,/,*)");
+        sc.nextLine();
+        String s = sc.nextLine();
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+    }
+}
