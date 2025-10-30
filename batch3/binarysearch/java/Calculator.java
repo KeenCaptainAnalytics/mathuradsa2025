f3cbda5b20f8aac6c4807441c3eeb07516db3b49
KeenCaptainAnalytics <kcalabs123@gmail.com>
2025-10-30 13:17:41 +0530

    Update Calculator.java

--- batch3/binarysearch/java/Calculator.java
+++ batch3/binarysearch/java/Calculator.java
@@ -16,17 +16,32 @@ public class Calculator {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first num");
-        int a = sc.nextInt();
-        System.out.println("Enter second number");
-        int b = sc.nextInt();
-        System.out.println("enter symbol (+,-,/,*)");
-        sc.nextLine();
-        String s = sc.nextLine();
+        while(true){
+            System.out.println("Enter first num");
+            int a = sc.nextInt();
+            System.out.println("Enter second number");
+            int b = sc.nextInt();
+            System.out.println("enter symbol (+,-,/,*)");
+            sc.nextLine();
+            String s = sc.nextLine();
 
 
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(s);
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(s);
+
+            switch(s){
+                case "+":System.out.println( add(a,b)+"====================");
+                        break;
+                case "-" :System.out.println(  diff(a, b)+"====================");
+                        break;
+                case "/" : System.out.println( div(a, b)+"====================");
+                        break;
+                case "*" :System.out.println( multiply(a, b)+"====================");
+                        break;
+                default : System.out.println("error");
+                
+            }
+    }
     }
 }
